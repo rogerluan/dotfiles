f7994fe4e6c647110b8d9b67ba9c2c8642704715
Roger Oba <rogerluan.oba@gmail.com>
2020-03-29 15:36:51 -0300

    Add Solarized Dark to Xcode.

---
+++ Xcode/install_xcode_solarized_dark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Shamelessly copied from https://stackoverflow.com/a/246128/4075379
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DESTDIR=~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+
+echo " * Installing Solarized Dark for Xcode. . ."
+mkdir -p $DESTDIR
+
+if [[ $1 = "-f" || $1 = "--force" || $1 = "force" ]]; then
+  rm $DESTDIR/Solarized\ Dark.xccolortheme
+fi
+
+# Note: although this creates a symlink, once this file is edited by Xcode,
+# Xcode creates its own copy for it, so it won't be version-controlled, unfortunately.
+ln -s $DIR/Solarized\ Dark.xccolortheme $DESTDIR/Solarized\ Dark.xccolortheme
+
+echo " * Done! Solarized Dark for Xcode has been installed. Select this theme from Xcode."
