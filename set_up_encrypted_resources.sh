a10fd8f96715320fbd180edc0072d3e587b3e6e4
Roger Oba <rogerluan.oba@gmail.com>
2020-05-02 20:02:08 -0300

    Add script set up encrypted resources (decrypting them).

---
+++ set_up_encrypted_resources.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KEY=$1
+
+function decrypt() {
+  SOURCE=$1
+  echo "Decrypting from '$SOURCE'"
+  # Extract to the same folder where it was encrypted from
+  openssl aes-256-cbc -d -in $SOURCE -k $KEY | tar -v --extract --gzip --file - -C /
+}
+
+SYSTEM_PATH="$HOME"
+REPO_PATH="$DOTFILES_DIR"
+
+# Decrypt all files with .encrypted extension in this folder.
+for ENCRYPTED_FILE in $(find . -name '*.encrypted'); do
+  decrypt "$REPO_PATH/$ENCRYPTED_FILE"
+done
