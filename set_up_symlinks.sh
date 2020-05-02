429225ae93652da0a6fa15f8b2d19ecdb572f320
Roger Oba <rogerluan.oba@gmail.com>
2020-05-02 20:18:24 -0300

    Fix symlink.

--- set_up_symlinks.sh
+++ set_up_symlinks.sh
@@ -7,8 +7,7 @@
 # DOTFILES_DIR may have not been initialized yet, if this is the first time setting up .zshrc
 source .exports
 
-ln -s $DOTFILES_DIR/.z $HOME/.z
 ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -s $DOTFILES_DIR/.zshenv $HOME/.zshenv
 ln -s $DOTFILES_DIR/.aliases $HOME/.aliases
-ln -s $DOTFILES_DIR/.aliases $HOME/.exports
+ln -s $DOTFILES_DIR/.exports $HOME/.exports
