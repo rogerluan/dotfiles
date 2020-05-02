2ca7ae6a4a07e0e4625bc8a5a4f8ffde5bf825b6
Roger Oba <rogerluan.oba@gmail.com>
2020-05-02 20:02:14 -0300

    Add script to set up symlinks.

---
+++ set_up_symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+################################################################################
+### Set up symbolic links
+################################################################################
+
+set -e # Immediately rethrows exceptions
+
+# DOTFILES_DIR may have not been initialized yet, if this is the first time setting up .zshrc
+source .exports
+
+ln -s $DOTFILES_DIR/.z $HOME/.z
+ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
+ln -s $DOTFILES_DIR/.zshenv $HOME/.zshenv
+ln -s $DOTFILES_DIR/.aliases $HOME/.aliases
+ln -s $DOTFILES_DIR/.aliases $HOME/.exports
