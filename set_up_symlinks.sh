f74dcbd19988da65d7bd60b1875352f54478fb95
Roger Oba <rogerluan.oba@gmail.com>
2020-07-11 00:46:16 -0300

    Add gemrc symlink.

--- set_up_symlinks.sh
+++ set_up_symlinks.sh
@@ -7,6 +7,7 @@
 # DOTFILES_DIR may have not been initialized yet, if this is the first time setting up .zshrc
 source .exports
 
+ln -s $DOTFILES_DIR/.gemrc $HOME/.gemrc
 ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
 ln -s $DOTFILES_DIR/.zshenv $HOME/.zshenv
 ln -s $DOTFILES_DIR/.aliases $HOME/.aliases
