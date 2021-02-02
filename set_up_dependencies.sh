0bc1f3aa844e55cf6edf515d60a92bb1557395c9
Roger Oba <rogerluan.oba@gmail.com>
2021-02-02 11:47:41 -0300

    Install more dependencies.

--- set_up_dependencies.sh
+++ set_up_dependencies.sh
@@ -38,3 +38,34 @@ echo " * Installing Heroku"
 brew tap heroku/brew
 brew install heroku
 echo " * Heroku installed successfully!"
+
+# Danger - https://danger.systems
+echo " * Installing Danger"
+brew install danger/tap/danger-swift
+echo " * Danger installed successfully!"
+
+# Prettier - https://prettier.io
+echo " * Installing Prettier"
+brew install prettier
+echo " * Prettier installed successfully!"
+
+# ktlint - https://github.com/pinterest/ktlint
+echo " * Installing ktlint"
+brew install ktlint
+echo " * ktlint installed successfully!"
+
+# Arena - https://github.com/finestructure/Arena
+echo " * Installing Arena"
+brew install finestructure/tap/arena
+echo " * Arena installed successfully!"
+
+# Arena - https://github.com/peripheryapp/periphery
+echo " * Installing Periphery"
+brew tap peripheryapp/periphery
+brew install periphery
+echo " * Periphery installed successfully!"
+
+# Make - https://www.gnu.org/software/make/manual/make.html
+echo " * Updating Make"
+brew install make
+echo " * Make updated successfully!"
