eeb2f0bf69d9bb553911ee115e20e7a6ac7d6f2f
Roger Oba <rogerluan.oba@gmail.com>
2021-05-01 18:17:22 -0300

    Unify Xcode setup script.

--- Xcode/install_xcode_code_snippets.sh
+++ Xcode/install_xcode_code_snippets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-### Moves Xcode Code Snippets from the repo to Xcode directory
+### Installs Xcode Code Snippets and Solarized Dark Theme
 ################################################################################
 
 FONT_AND_COLOR_THEMES_DIR="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
@@ -9,3 +9,10 @@ CODE_SNIPPETS_DIR="$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
 
 cp -r $DOTFILES_DIR/Xcode/FontAndColorThemes $FONT_AND_COLOR_THEMES_DIR
 cp -r $DOTFILES_DIR/Xcode/CodeSnippets $CODE_SNIPPETS_DIR 
+
+# Set Xcode appearance to Dark Mode
+defaults write com.apple.dt.Xcode IDEAppearance -int 2
+
+# Select Solarized Dark as the current dark theme in Xcode.
+defaults write com.apple.dt.Xcode XCFontAndColorCurrentDarkTheme -string "Solarized Dark.xccolortheme"
+
