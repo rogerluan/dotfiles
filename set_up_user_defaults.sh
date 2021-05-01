c3647f3588a138c1e037f562bea2a41fb0a1786b
Roger Oba <rogerluan.oba@gmail.com>
2021-04-30 22:20:26 -0300

    Also enable click with tap in the login screen.

--- set_up_user_defaults.sh
+++ set_up_user_defaults.sh
@@ -109,6 +109,9 @@ defaults write com.apple.Spotlight lastWindowPosition -string "{{987, 585}, {680
 
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 1
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 1
 
