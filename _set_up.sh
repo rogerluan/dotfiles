aa37c34dcb8e2261bb31db85bcd57dfb5ebda5be
Roger Oba <rogerluan.oba@gmail.com>
2022-03-29 14:23:50 +0200

    Fix order of the commands.

--- _set_up.sh
+++ _set_up.sh
@@ -5,9 +5,10 @@
 ################################################################################
 
 source .exports
+source set_up_symlinks.sh
+source ~/.zshrc
 source set_up_dependencies.sh
 source set_up_commit_signing.sh
-source set_up_symlinks.sh
 source set_up_encrypted_resources.sh $1
 source set_up_user_defaults.sh
 source Terminal/set_up_terminal.sh
