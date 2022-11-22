4583a09b185ee62841bc8664eb303aa5954f10e7
Roger Oba <rogerluan.oba@gmail.com>
2022-11-22 14:48:53 -0800

    Fix issue where rbenv wasn't available yet.

--- set_up_dependencies.sh
+++ set_up_dependencies.sh
@@ -17,10 +17,12 @@ source ~/.zshrc
 # Ruby - https://www.ruby-lang.org/en/
 echo " * Installing Ruby"
 # Will pick up version from ~/.ruby-version
+
+# Installs all dependencies declared in Brewfile
+brew bundle
+
 RUBY_VERSION="$(cat ~/.ruby-version)"
 rbenv install
 rbenv global $RUBY_VERSION
 echo " * Ruby $RUBY_VERSION installed successfully!"
 
-# Installs all dependencies declared in Brewfile
-brew bundle
