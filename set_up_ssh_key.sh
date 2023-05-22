a26c41bc50ecf04575c431eb74e3392985beccfc
Roger Oba <rogerluan.oba@gmail.com>
2023-05-22 17:00:57 -0300

    Add better support for Ubuntu machines.

--- set_up_ssh_key.sh
+++ set_up_ssh_key.sh
@@ -26,18 +26,39 @@ ssh-keygen -t rsa -b 4096 -C "$email"
 # Add your SSH key to the ssh-agent
 # Start the ssh-agent in the background
 eval "$(ssh-agent -s)"
-# Automatically load keys into the ssh-agent and store passphrases in the keychain
+
+# Automatically load keys into the ssh-agent and store passphrases in the keychain (when in macOS)
 # Host *
 #   AddKeysToAgent yes
 #   UseKeychain yes
 #   IdentityFile ~/.ssh/id_rsa
-printf "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS-specific commands here
+  printf "Host *\n  AddKeysToAgent yes\n  UseKeychain yes\n  IdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
+else
+  # Linux-specific commands here
+  printf "Host *\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
+fi
 
-# Add your SSH private key to the ssh-agent and store your passphrase in the keychain
-ssh-add -K ~/.ssh/id_rsa
+# Add your SSH private key to the ssh-agent and store passphrase in the keychain (when in macOS)
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS-specific commands here
+  ssh-add -K ~/.ssh/id_rsa
+else
+  # Linux-specific commands here
+  ssh-add ~/.ssh/id_rsa
+fi
 
 # Copy the contents of the id_rsa.pub file to clipboard
 cat ~/.ssh/id_rsa.pub | toClipboard
 echo "Copied SSH key to clipboard!"
-echo "Opening Safari, create a descriptive title to describe this computer and paste the key there"
-open -a safari https://github.com/settings/ssh/new
+
+# Open the SSH key URL in the default web browser
+if command -v open > /dev/null; then
+  open "https://github.com/settings/ssh/new"
+elif command -v xdg-open > /dev/null; then
+  xdg-open "https://github.com/settings/ssh/new"
+else
+  echo "Please open the following URL in your web browser and paste the SSH public key:"
+  echo "https://github.com/settings/ssh/new"
+fi
